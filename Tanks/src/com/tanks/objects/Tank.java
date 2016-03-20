043e7f5cd72374d93226f655644d946c9c7cf6b0
Ken Malavisuriya <kmal364@aucklanduni.ac.nz>
2016-03-21 12:33:07 +1300

    added respawning object and changed states

--- Tanks/src/com/tanks/objects/Tank.java
+++ Tanks/src/com/tanks/objects/Tank.java
@@ -9,6 +9,9 @@ public class Tank extends GameObject {
 	private Missle missle;
 	private ArrayList<Missle> missles;
 	private ArrayList<Reminder> reminder;
+	
+	private static int bullet_delay = 30;
+	private static int time = 0;
 
 	public Tank(int x, int y, int speed, int width, int height) {
 		super(x, y, speed, width, height);
@@ -17,14 +20,18 @@ public class Tank extends GameObject {
 	}
 	
 	public void move() {
+		time = time - 1;
 		x += Math.round(path * Math.cos(a * Math.PI/180));
 		y += Math.round(path * Math.sin(a * Math.PI/180));
 	}
 	
 	public void fire() {
+		if (time <= 0) {
 		missle = new Missle(x+width/2,y+height/2, 5, 10, 10,a);
 		missles.add(missle);
 		reminder.add(new Reminder(2, missle));
+		time = bullet_delay;
+		}
 	}
 	
 	public ArrayList<Reminder> getReminder() {
--- Tanks/src/com/tanks/states/GameState.java
+++ Tanks/src/com/tanks/states/GameState.java
@@ -6,6 +6,7 @@ import java.awt.Graphics2D;
 import java.awt.Rectangle;
 import java.awt.geom.AffineTransform;
 import java.util.ArrayList;
+import java.util.Random;
 
 import com.tanks.objects.Missle;
 import com.tanks.objects.Tank;
@@ -14,19 +15,19 @@ public class GameState extends State{
 	
 	private Tank tank;
 	private Missle missle;
+	ArrayList<Tank> enemy;
 	
 	public GameState(Tank tank) {
 		this.tank = tank;
+		enemy = new ArrayList<Tank>();
+		spawnEnemy();
 	}
-	
-	public GameState() {
-		this.tank = null;
-	}
-	
 	
 	@Override
 	public void doDrawing(Graphics g) {
 		Graphics2D g2d = (Graphics2D) g;	
+		
+		// Player Tank
 		Rectangle temp = new Rectangle(tank.getX(),tank.getY(),tank.getWidth(),tank.getHeight());
 		g2d.setColor(Color.green);
 		AffineTransform oldTransform = g2d.getTransform();
@@ -36,6 +37,7 @@ public class GameState extends State{
 	    g2d.fillRect(tank.getX()+tank.getWidth(), tank.getY()+5, 10, 40);
 	    g2d.setTransform(oldTransform);
 	    
+	    // Missles
 	    ArrayList<Missle> ms = tank.getMissle();
 	    
 	    for (int i = 0; i < ms.size(); i++) {
@@ -44,7 +46,13 @@ public class GameState extends State{
 	    	g2d.fillRect(missle.getX(),missle.getY(),missle.getWidth(),missle.getHeight());
 	    	
 	    }
-}
+	    
+	    // Enemy Tank
+	    g2d.setColor(Color.black);
+	    if (enemy.size() != 0) {
+	    	g2d.fillRect(enemy.get(0).getX(), enemy.get(0).getY(), enemy.get(0).getWidth(), enemy.get(0).getHeight());
+	    }
+	}
 
 	@Override
 	public void tick() {
@@ -59,6 +67,32 @@ public class GameState extends State{
 			if (missle.getVis() == false) {
 				ms.remove(i);
 			}
-		}	
+			if (enemy.size() != 0) {
+				if (collision(enemy.get(0),missle)) {
+					enemy.remove(0);
+					ms.remove(i);
+					spawnEnemy();
+				}
+			}
+		}
+	}
+	
+	private void spawnEnemy() {
+		Random rand = new Random();
+		// Fix the bounds
+		int x = rand.nextInt(1024-50) + 50;
+		int y = rand.nextInt(768-50) + 50;
+		enemy.add(new Tank(x,y,8, 50, 50));
+	}
+	
+	private boolean collision(Tank getrekt, Missle missle) {
+		Rectangle enemyBounds = new Rectangle(getrekt.getX(), getrekt.getY(), getrekt.getWidth(), getrekt.getHeight());
+		Rectangle missleBounds = new Rectangle(missle.getX(), missle.getY(), missle.getWidth(), missle.getHeight());
+		
+		if (enemyBounds.intersects(missleBounds)) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 }
